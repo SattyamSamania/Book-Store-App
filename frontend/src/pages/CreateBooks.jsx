21ca7645a11021c3769708057673c88da5f2ea70
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-02-18 21:18:11 +0530

    Updated CreateBooks Section

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const CreateBooks = () => {
+
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [publishYear, setPublishYear] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
+    const handlSaveBook = ()=> {
+        const data ={ 
+            title,
+            author, 
+            publishYear,
+        };
+        setLoading(true);
+        axios
+        .post('http://localhost:5555/books', data)
+        .then(()=> {
+            setLoading(false);
+            navigate('/');
+        })
+        .catch((error) => {
+            setLoading(false);
+            alert('An error happened. Please Check console')
+            console.log(error);
+        })
+    }
   return (
     <div>CreateBooks</div>
   )

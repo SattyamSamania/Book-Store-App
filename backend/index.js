8511189725bd33240ff2dc7abbec09e38eee639d
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-02-18 11:31:28 +0530

    Add Cors Policy

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 import bookRoute from './routes/bookRoute.js'
+import cors from 'cors';
 
 const app = express();
 
@@ -10,6 +11,19 @@ const app = express();
 
 // Middleware for parsing request body 
 app.use(express.json());
+
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 
 // Get Request 

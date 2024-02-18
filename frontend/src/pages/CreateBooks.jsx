8d8b00651e073108920b241174ee3663fa867d9b
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-02-18 22:47:03 +0530

    Updated CRUD Operations on Frontend

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -3,6 +3,7 @@ import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import { useSnackbar } from 'notistack'
 
 const CreateBooks = () => {
 
@@ -12,6 +13,8 @@ const CreateBooks = () => {
     const [loading, setLoading] = useState(false);
 
     const navigate = useNavigate();
+
+    const {enqueueSnackbar} = useSnackbar();
     const handlSaveBook = ()=> {
         const data ={ 
             title,
@@ -23,11 +26,13 @@ const CreateBooks = () => {
         .post('http://localhost:5555/books', data)
         .then(()=> {
             setLoading(false);
+            enqueueSnackbar('Book Created Successfully', {variant: 'success'})
             navigate('/');
         })
         .catch((error) => {
             setLoading(false);
             alert('An error happened. Please Check console')
+            enqueueSnackbar('Error', {variant: 'error'})
             console.log(error);
         })
     }

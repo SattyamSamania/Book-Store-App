c860e9fe2f47b09dc7137b8a3589d9f340e1b7dd
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-02-18 09:34:07 +0530

    Add Database Setup for the App

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,5 @@
 export const PORT = '5555'
 
+
+export const mongoDBURL = 'mongodb+srv://sattyamsamania2810:5w29Se8SxpzBg8ff@cluster1.1gz8cvs.mongodb.net/books-collection?retryWrites=true&w=majority'
+
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,25 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.listen(PORT, ()=> {
-    console.log(`Server is Running on Port ${PORT}`);
+
+// Get Request 
+app.get('/', (req, res)=> {
+    res.send('MERN Stack Project')
+})
+
+
+
+
+mongoose.connect(mongoDBURL)
+.then(() => {
+    console.log('App Connected to Database');
+    app.listen(PORT, ()=> {
+        console.log(`Server is Running on PORT ${PORT}`);
+    });
+})
+.catch((error)=> {
+    console.log(error);
 })

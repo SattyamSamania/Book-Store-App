147e100e21d74c63b9bb54cfc856d00bbe528b36
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-02-18 11:21:13 +0530

    Add Delete route for Deleting the Books

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { response } from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
+import bookRoute from './routes/bookRoute.js'
 
 const app = express();
 
@@ -16,94 +17,8 @@ app.get('/', (req, res)=> {
     res.send('MERN Stack Project')
 })
 
+app.use('/books', bookRoute)
 
-// Route for Save a new Book
-app.post('/books', async (req, res) => {
-    try {
-      if (
-        !req.body.title ||
-        !req.body.author ||
-        !req.body.publishYear
-      ) {
-        return res.status(400).send({
-          message: 'Send all required fields: title, author, publishYear',
-        });
-      }
-      const newBook = {
-        title: req.body.title,
-        author: req.body.author,
-        publishYear: req.body.publishYear,
-      };
-  
-      const book = await Book.create(newBook);
-  
-      return res.status(201).send(book);
-    } catch (error) {
-      console.log(error.message);
-      res.status(500).send({ message: error.message });
-    }
-  });
-
-//   Route for Get All Books from the Database 
-app.get('/books', async (req, res) => {
-    try {
-        const books = await Book.find({});
-        return res.status(200).json({
-            count: books.length,
-            data: books
-        })
-        
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({message: error.message});
-        
-    }
-})
-
-// Route for Get One Book from database by id 
-app.get('/books/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-
-        const book = await Book.findById(id);
-        return res.status(200).json(book);
-        
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({message: error.message});
-        
-    }
-})
-  
-
-
-// Route for Update a Book
-app.put('/books/:id', async (req, res) => {
-    try {
-      if (
-        !req.body.title ||
-        !req.body.author ||
-        !req.body.publishYear
-      ) {
-        return res.status(400).send({
-          message: 'Send all required fields: title, author, publishYear',
-        });
-      }
-  
-      const { id } = req.params;
-  
-      const result = await Book.findByIdAndUpdate(id, req.body);
-  
-      if (!result) {
-        return res.status(404).json({ message: 'Book not found' });
-      }
-  
-      return res.status(200).send({ message: 'Book updated successfully' });
-    } catch (error) {
-      console.log(error.message);
-      res.status(500).send({ message: error.message });
-    }
-  });
 
 mongoose.connect(mongoDBURL)
 .then(() => {
